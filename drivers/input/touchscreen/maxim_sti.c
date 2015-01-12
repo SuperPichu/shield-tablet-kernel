32bad5a7576fb5be08263c9ef348ecb1289867ab
Chris Midkiff <chrismidkiff15@gmail.com>
2015-01-12 13:20:23 -0500

    First attempt at enabling DTTW

--- drivers/input/touchscreen/maxim_sti.c
+++ drivers/input/touchscreen/maxim_sti.c
@@ -46,7 +46,7 @@
 #define INPUT_ENABLE_DISABLE        0
 #define FB_CALLBACK                 1
 #define SUSPEND_POWER_OFF           0
-#define DOUBLE_TAP                  0
+#define DOUBLE_TAP                  1
 #define NV_ENABLE_CPU_BOOST         1
 #define NV_STYLUS_FINGER_EXCLUSION  0
 
